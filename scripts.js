a1d5bd9dd4ba533e577a5e7692670960a89b5496
Morgan <morgan-graham@live.com>
2020-10-13 11:04:55 -0700

    menu working

--- scripts.js
+++ scripts.js
@@ -4,17 +4,39 @@
 */
 $(function () {
   $(window).on('scroll', function () {
-      if ( $(window).scrollTop() > 150 ) {
-          $('.navbar').addClass('active');
-      } else {
-          $('.navbar').removeClass('active');
+    if ($(window).scrollTop() > 150) {
+      $('.navbar').addClass('active');
+      $('.navbar .navbar-brand > img').removeClass('hide-logo');
+      $('.navbar .navbar-brand > img').addClass('display-logo');
+    } else {
+      $('.navbar').removeClass('active');
+      if (!($(".navbar-collapse.show")[0])) {
+        $('.navbar').removeClass('menu-displayed');
+        $('.navbar .navbar-brand > img').removeClass('display-logo');
+        $('.navbar .navbar-brand > img').addClass('hide-logo');
       }
+    }
   });
 });
 
 /* make navbar expand w/styling even when transparent */
-// $(".navbar-toggler").click(function () {
-//   $("nav").toggleClass("menu-displayed");
-//   /* show logo */
-//   $('.navbar-brand > img').toggle(0);
-// })
+$(".navbar-toggler").click(function () {
+  $("nav").toggleClass("menu-displayed");
+  $('.navbar .navbar-brand > img').removeClass('hide-logo');
+  $('.navbar .navbar-brand > img').addClass('display-logo');
+  /* show logo */
+  // $('.navbar-brand > img').toggle(0);
+})
+
+
+// $(function () {
+//   $(window).on('scroll', function () {
+//     if ($(".navbar-collapse.show")[0]) {
+//       // Do something if class exists
+//       $("nav").toggleClass("menu-displayed");
+//       console.log("show");
+//     } else {
+//       // Do something if class does not exist
+//     }
+//   })
+// });
