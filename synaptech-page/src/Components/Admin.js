00c640d7520b7a6b3b6ed5e114eb6f7a6aad57cd
hannah627e <hannah06@uw.edu>
2022-04-23 16:12:23 -0700

    adjusted our team page for mobile

--- synaptech-page/src/Components/Admin.js
+++ synaptech-page/src/Components/Admin.js
@@ -4,19 +4,28 @@ import "../styles/OurTeamStyles.css";
 function Admin(props) {
 
   return(
-    <div className={"AdminContainer"} Style = {"width: 30vw; display: flex; flex-direction: row; justify-content: space-around; padding: 2rem;"}>
+    // original styling: width: 30vw; display: flex; flex-direction: row; justify-content: space-around; padding: 2rem;
+    <div className={"AdminContainer"} Style = {"display: flex; flex-direction: row; justify-content: space-around; padding: 2rem;"}>
 
       <div Style = {"flex-direction: column; display: flex; align-items: center;"}>
-        <img Style = {"border-radius:100vh; height: 15vw; width: 15vw"} src={props.image} alt={props.name} />
-        <h3>{props.major}</h3>
-        <h4>{props.minor}</h4>
+        <img Style = {"border-radius:100vh; height: 25vw; width: 25vw"} src={props.image} alt={props.name} />
+        <div Style = {"text-align: center"}>
+          <h3 Style = {"font-size: 1.4rem"}>{props.major}</h3>
+          <h4 Style = {"font-size: 1rem"}>{props.minor}</h4>
+        </div>
       </div>
 
-      <div Style = {"flex-direction: column; display: flex; align-items: flex-end"}>
+    {/* <div Style = {"flex-direction: column; display: flex; align-items: flex-end"}>
         <h1 Style = {"padding-left: 1rem;"}>{props.name}</h1>
         <div className="hr"><p></p></div>
         <h2 Style = {"color: #9E9577"}>{props.title}</h2>
         <p Style = "padding-left: 1rem; width:25vw;">{props.blurb}</p>
+      </div> */}
+      <div Style = {"flex-direction: column; display: flex;"}>
+        <h1 Style = {"padding-left: 1rem;"}>{props.name}</h1>
+        <div className="hr"><p></p></div>
+        <h2 Style = {"padding-left: 1rem; color: #9E9577"}>{props.title}</h2>
+        <p Style = "padding-left: 1rem;">{props.blurb}</p>
       </div>
 
     </div>
